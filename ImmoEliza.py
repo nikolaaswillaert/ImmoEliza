8450bd6a3296c5a66e4c64635de251be5dd6b349
NikolaasWillaert <nikolaas.willaert@gmail.com>
2023-06-26 10:51:04 +0200

    added ipynb to test code

---
+++ ImmoEliza.py
@@ -0,0 +1,23 @@
+# https://www.immoscoop.be/
+
+
+'''
+Locality
+Type of property (House/apartment)
+Subtype of property (Bungalow, Chalet, Mansion, ...)
+Price
+Type of sale (Exclusion of life sales)
+Number of rooms
+Living Area
+Fully equipped kitchen (Yes/No)
+Furnished (Yes/No)
+Open fire (Yes/No)
+Terrace (Yes/No)
+- If yes: Area
+Garden (Yes/No)
+- If yes: Area
+Surface of the land
+Surface area of the plot of land
+Number of facades
+Swimming pool (Yes/No)
+State of the building (New, to be renovated, ...)'''

d67725e213af83c733565e241f3b0f448368e6d3
unknown <sam.hoste@hotmail.com>
2023-06-30 09:35:55 +0200

    fixed error from last commit

--- utils/get_dataset.py
+++ utils/get_dataset.py
@@ -5,6 +5,7 @@ import json
 from concurrent.futures import ThreadPoolExecutor,ProcessPoolExecutor, as_completed
 import time
 import pandas as pd
+import sys
 import threading
 
 # Function to scrape URLs
@@ -25,15 +26,17 @@ def scrape_urls(page_num):
 
 def thread_scraping():
     full_list_url = []
-    num_pages = 333
+    num_pages = 2
 
     # Create a list to store threads
     threads = []
     start_time = time.time()  # Start timer
+    print("Scraping individual pages...")
     
     # Create and start threads
     for i in range(1, num_pages + 1):
         t = threading.Thread(target=lambda: full_list_url.extend(scrape_urls(i)))
+        reporting("Search pages scraped:", i)
         threads.append(t)
         t.start()
 
@@ -43,19 +46,33 @@ def thread_scraping():
 
     end_time = time.time()  # Stop timer
     execution_time = end_time - start_time
-
-    print("Scraping completed!")
+    print("Scraping completed!              ")
     print("Total URLs scraped:", len(full_list_url))
     print("Total time:", execution_time, "seconds")
     return full_list_url
+
+def reporting(str, i): 
+    sys.stdout.write(str + ' %d\r' %i)
+    sys.stdout.flush()
+    return
+
+def counter():
+    global counters 
+    if counters < 1: 
+        counters = 1
+    else:
+        counters +=1
+    return
 
 def scrape_house(url):
     """Scrapes all the info from a house listing"""
 
     # Get the house listing and make a soup
-    house_page = requests.get(url)
-    house_page = BeautifulSoup(house_page.text, 'html.parser')
-    final_dictionary = {}
+    try:
+        house_page = requests.get(url)
+        house_page = BeautifulSoup(house_page.text, 'html.parser')
+    except: 
+        final_dictionary = {}
 
     # Get the hidden info from the java script
     regex = r"window.classified = (\{.*\})"
@@ -164,43 +181,35 @@ def scrape_house(url):
 # CALL THIS FUNCTION IF NOT FULL_LIST_20k.txt available houses_links = thread_scraping()
 def create_dataframe():
     houses_links = []
-    with open(r".\data_output\full_list_20k.txt", "r") as f:
+    houses_links = thread_scraping()
+    
+    """with open("./full_list_20k.txt", "r") as f:
         count = 0
-        for i in f:
-            if count < 300:
-                houses_links.append(i)
-                print(houses_links[count])
-                count+=1
+        for url in f:
+            if count < 3000:
+                houses_links.append(url)
+                count +=1
             else:
-                break
+                 break"""
 
+    print("")
+    print("Scraping individual pages...")
+    start_time = time.time()  # Start timer
     with ThreadPoolExecutor(max_workers=10) as executor:
-        futures = [executor.submit(scrape_house, url) for url in houses_links]
-        results =  [item.result() for item in futures]
+        #try:
+        futures = [(executor.submit(scrape_house, url), counter(), reporting("Individual pages scraped:", counters), time.sleep(.2)) for url in houses_links]
+        results =  [item[0].result() for item in futures]
         df = pd.DataFrame(results)
+    
+    df.to_csv(r'.\data_output\dataframe.csv', index = True)
+
+    end_time = time.time()  # Stop timer
+    execution_time = end_time - start_time
+
+    print("Scraping completed!                        ")
+    print("Total time spent scraping:", execution_time, "seconds")
     return df
 
-df = create_dataframe()
-df.to_csv(r'.\data_output\dataframe.csv', index=True)
+counters = 1
 
-print(df)
 
-df.loc[df['kitchen'] == 'NOT_INSTALLED', 'kitchen'] = 0
-df.loc[df['kitchen'] == 'INSTALLED', 'kitchen'] = 1
-
-df.loc[df['furnished'] == None, 'furnished'] = 0
-df.loc[df['furnished'] == 'None', 'furnished'] = 0
-df.loc[df['furnished'] == False, 'furnished'] = 0
-df.loc[df['furnished'] == True, 'furnished'] = 1
-
-df.loc[df['fireplace'] == None, 'fireplace'] = 0
-df.loc[df['fireplace'] == 'None', 'fireplace'] = 0
-
-df.loc[df['terrace'] == False, 'terrace'] = 0
-df.loc[df['terrace'] == True, 'terrace'] = 1
-
-df.loc[df['terrace_area'] == 'NaN', 'terrace_area'] = 0
-
-print(df)
-
-df.fillna

1ddb72b24364aa4952c69d9ed2bc609668ef2415
unknown <sam.hoste@hotmail.com>
2023-06-30 13:44:29 +0200

    Added errorhandling to thread_scraping() and changed to relative paths

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import utils.get_dataset as getdata
 import utils.clean_dataset as cleandata
 import pandas as pd
 
-#dataset = getdata.create_dataframe()
-#cleandata.clean_dataset(dataset)
-df = pd.read_csv("/home/niko/Desktop/GitHub Projects/ImmoEliza/data_output/dataframe_20k_280623.csv")
-a = cleandata.clean_dataset(df)
-print(a)
+dataset = getdata.create_dataframe()
+df = pd.read_csv(r".\data_output\dataframe.csv")
+#df = pd.read_csv("/home/niko/Desktop/GitHub Projects/ImmoEliza/data_output/dataframe_20k_280623.csv")
+cleaned_csv = cleandata.clean_dataset(df)
+print(cleaned_csv)
